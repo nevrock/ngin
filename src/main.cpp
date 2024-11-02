c2de7bcefdc7ae0ae1768a840320f3e212bcfa1b
traven <travenrockblaney@gmail.com>
2024-11-01 22:39:39 -0700

    first major commit

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,7 @@
 #include "imgui_impl_opengl3.h"
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <GLES2/gl2.h>
-#endif
+
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
