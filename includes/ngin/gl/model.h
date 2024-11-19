860176252da8d081562007d11c94c1f8aefe235c
traven <travenrockblaney@gmail.com>
2024-11-18 20:21:17 -0800

    some more ups

--- includes/ngin/gl/model.h
+++ includes/ngin/gl/model.h
@@ -31,7 +31,7 @@ public:
     
     virtual glm::mat4 getTransformation() {
         IObject* p = getParent();
-        return getPoint().getModelMatrix();
+        //return getPoint().getModelMatrix();
         if (p == nullptr) {
             return getPoint().getModelMatrix();
         }
